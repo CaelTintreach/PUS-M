8512666e7ebb0b2a21cfa81f446a64a6b1ef8a18
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-08-07 15:01:31 +0100

    Adding further test functionality.

--- tests/test_back_end.py
+++ tests/test_back_end.py
@@ -82,7 +82,7 @@ class TestViews(TestBase):
 
 	def test_adding_userstory(self):
 		with self.client:
-			response = self.client.post(url_for('adduserstory'), data=dict(userstoryName = "New Story", userstoryDesc= "New Story Desc", userstoryproject="1"), follow_redirects=True)
+			response = self.client.post(url_for('adduserstory'), data=dict(userstoryName = "New Story", userstoryDesc= "New Story Desc", userstoryProject="1"), follow_redirects=True)
 			self.assertIn(b"New Story", response.data)
 
 	def test_delete_project(self):
